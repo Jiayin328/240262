1aba9c805f0c19118b10a7c6abe016078b8d53c8
Jia Yin <31919741+Jiayin328@users.noreply.github.com>
2017-09-29 10:58:17 +0800

    Add files via upload
    Exercise Week01-01

---
+++ MyThread.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package countthread;
+
+/**
+ *
+ * @author Carynne
+ */
+public class MyThread extends Thread {
+
+    public static void main(String[] args) {
+        new Thread(new MyThread()).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int x = 1; x <= 10500; x++) {
+                System.out.println(x);
+                sleep(5000);
+                if (x%50==0){
+                    System.out.println("Hello World");
+                }
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
